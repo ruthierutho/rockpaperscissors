6fb97634414e28e1232cb90fd43d57323ef10781
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-11-01 15:22:56 +0000

    shows example players before inputting new names and moves

--- app/controller.py
+++ app/controller.py
@@ -37,6 +37,19 @@ def gameplay():
     playing_game = play_game(player1, player2)
     return render_template('index.html', players=players, winner=playing_game)
 
+# @app.route('/choose-move', methods=['POST'])
+# def choose_move():
+#     new_name1 = request.form['name1']
+#     new_move1 = request.form ['move1']
+#     new_name2 = request.form['name2']
+#     new_move2 = request.form ['move2']
+#     new_player1 = Player(name=new_name1, move=new_move1)
+#     new_player2 = Player(name=new_name2, move=new_move2)
+#     add_new_player(new_player1)
+#     add_new_player(new_player2)
+#     playing_game = play_game(new_player1, new_player2)
+#     return render_template('index.html', players=players, winner=playing_game)
+
 @app.route('/choose-move', methods=['POST'])
 def choose_move():
     new_name1 = request.form['name1']
@@ -45,11 +58,10 @@ def choose_move():
     new_move2 = request.form ['move2']
     new_player1 = Player(name=new_name1, move=new_move1)
     new_player2 = Player(name=new_name2, move=new_move2)
-    add_new_player(new_player1)
-    add_new_player(new_player2)
+    new_players = [new_player1, new_player2]
+   
     playing_game = play_game(new_player1, new_player2)
-    return render_template('index.html', players=players, winner=playing_game)
-
+    return render_template('index.html', new_players=new_players, winner=playing_game)
 
 
 
